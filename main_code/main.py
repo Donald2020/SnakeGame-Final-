778258ee90096e125921760d742a584ccb3348d8
Detravious Brinkley <d.brinkley97@gmail.com>
2019-12-10 19:12:28 -0500

    Update main.py

--- main_code/main.py
+++ main_code/main.py
@@ -61,12 +61,12 @@ class Master(Canvas):
             self.current.begin()
             self.running = 1
             
-     def tutorial(self):
-        """This takes you to the tutorial page"""
-        if self.running == 0:
-            self.configure(width=width, height=height, bg=tutorialpage)
-            root = tk.Tk()
-            tk.Label(root, 
+    def tutorial(self):
+	"""This takes you to the tutorial page"""
+	if self.running == 0:
+	    self.configure(width=width, height=height, bg=tutorialpage)
+	    root = tk.Tk()
+	    tk.Label(root, 
 		     text="Turtorial\n\n This is the snake game the rules are simiple:\n Rule #1: eat the food particles to score\n Rule #2: avoid eating yourself and hittig the boarder",
 		     fg = "red",
 		     font = "Times").pack()
